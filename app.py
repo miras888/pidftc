c671e67d07c048de3ac62622936b1b48d64a42ad
miras888 <125230902+miras888@users.noreply.github.com>
2025-04-05 21:51:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, flash, redirect, render_template, request, session, sen
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup
-from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
