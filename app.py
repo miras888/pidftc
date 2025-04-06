f2760eb63ada7aa877b12a2e83e81779d7fb785e
miras888 <125230902+miras888@users.noreply.github.com>
2025-04-06 11:21:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,35 +15,25 @@ db = SQL("sqlite:///pid.db")
  
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-     # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
         
-
-        # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
@@ -51,10 +41,8 @@ def login():
 def logout():
     """Log user out"""
 
-    # Forget any user_id
     session.clear()
 
-    # Redirect user to login form
     return redirect("/login")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -114,10 +102,8 @@ def models():
 def pidpress():
     categories = db.execute("SELECT DISTINCT category FROM news")
 
-    # Get the selected category from the URL parameters (if any)
     selected_category = request.args.get('category', 'All')
 
-    # Fetch news articles based on the selected category
     if selected_category == 'All':
         news_articles = db.execute("SELECT * FROM news")
     else:
@@ -185,7 +171,6 @@ def qa():
             flash("Your question has been posted!", "success")
             return redirect("/qa")
 
-    # Handle GET and also reload on POST failure
     selected_category = request.args.get("category", "All")
     categories = db.execute("SELECT DISTINCT category FROM questions")
 
